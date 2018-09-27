8cc02fc54d2eb6c6298efdcc8396e9c332f60801
whdesigns <43645293+whdesigns@users.noreply.github.com>
2018-09-27 15:55:49 +0100

    Create bot.py
    Added some code to show the use of escape characters.

---
+++ 1-basics/1-output/3-escape-characters/bot.py
@@ -0,0 +1,10 @@
+# Display escape characters 
+print("\n Display a new line") 
+print("\t Display a tab space")
+print("\\Display a back slash")
+print("\' Display a single quote.")
+
+
+
+# Display a message using multiple escape counters
+print("\n\t" + "\" Go ahead make my day. Sudden Impact \"")
