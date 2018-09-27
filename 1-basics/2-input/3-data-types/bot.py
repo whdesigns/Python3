599b45a0a7825f6b724adf7a0b7cdec98deb5001
whdesigns <43645293+whdesigns@users.noreply.github.com>
2018-09-27 17:37:37 +0100

    Create bot.py
    Added code to demonstrate the use of data types.

---
+++ 1-basics/2-input/3-data-types/bot.py
@@ -0,0 +1,16 @@
+# Read user data 
+print("Please enter your name")
+name = input()
+
+print("Please enter your age")
+age = int(input() )
+
+print("Please enter your height (m)")
+height = input() 
+height = float( input()) 
+
+print("Please enter your weight (kg)")
+weight = float( input() ) 
+
+bmi  = weight / (height * height)  
+print(name, "your bmi is", bmi)
