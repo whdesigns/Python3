6e4c356f00db116d089432c6285a2af2dc8ce49c
William Hughes <43645293+whdesigns@users.noreply.github.com>
2018-10-04 16:22:48 +0100

    Create bot.py
    We added numbers to determine comparisons.

---
+++ 1-basics/3-decision/5-decision-comparison/bot.py
@@ -0,0 +1,12 @@
+print("Please enter a number!")
+first_number = float(input())
+
+print("Please enter second number!")
+second_number = float(input())
+
+if second_number > first_number:
+  print("The second number is bigger!")
+  elif (first_number < second_number): 
+    print("The first is bigger!")
+    else: 
+      print("Both numbers are equal")
