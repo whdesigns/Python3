20ad0f9c0e0452f18633ef9ee9ec3b637268ef89
William Hughes <43645293+whdesigns@users.noreply.github.com>
2018-10-04 15:40:48 +0100

    Create bot.py
    Added if, else, and elif statements to determine which direction the user should go.

---
+++ 1-basics/3-decision/3-if-elif-else/bot.py
@@ -0,0 +1,19 @@
+# Read direction from user
+print("Please enter one of the following directions:")
+print("[w] to go up")
+print("[s] to move down")
+print("[a] to move left")
+print("[d] to move right")
+
+direction = input()
+# Work out which direction move input
+if (direction == "w"):
+print("I am moving up!")
+elif (direction == "s"):
+print("I am moving down!")
+elif (direction == "a"):
+print("I am moving left!")
+elif (direction == "d"):
+  print("I am moving right!")
+else: 
+  print("I don't know what direction that is!")
