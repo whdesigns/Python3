ad3bb4abfb4a7480fc20a4dd500d3f211bb8bb18
William Hughes <43645293+whdesigns@users.noreply.github.com>
2018-10-09 17:56:40 +0100

    Update bot.py

--- 1-basics/1-output/3-escape-characters/bot.py
+++ 1-basics/1-output/3-escape-characters/bot.py
@@ -1,7 +1,7 @@
 # Display escape characters 
 print("\n Display a new line") 
 print("\t Display a tab space")
-print("\\Display a back slash")
+print("\\ Display a back slash")
 print("\' Display a single quote.")
 
 
