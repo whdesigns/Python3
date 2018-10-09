89f9c45d439003ba10dca1b6f941099f75c240ba
William Hughes <43645293+whdesigns@users.noreply.github.com>
2018-10-09 18:24:30 +0100

    Update bot.py

--- 1-basics/3-decision/5-decision-comparison/bot.py
+++ 1-basics/3-decision/5-decision-comparison/bot.py
@@ -6,7 +6,7 @@ second_number = float(input())
 
 if second_number > first_number:
   print("The second number is bigger!")
-  elif (first_number < second_number): 
-    print("The first is bigger!")
-    else: 
-      print("Both numbers are equal")
+elif first_number < second_number: 
+  print("The first is bigger!")
+else: 
+  print("Both numbers are equal")
