8370c8adfd4048728f7e41a9cb23933a284b017f
William Hughes <43645293+whdesigns@users.noreply.github.com>
2018-10-10 19:56:28 +0100

    Update bot.py
    A while loop to count from 1-10 in ascii art.

--- 1-basics/4-repetition1/1-while-loop/bot.py
+++ 1-basics/4-repetition1/1-while-loop/bot.py
@@ -1,10 +1,9 @@
-NUM_FACES = 100
+NUM_FACES = 10
 
-for count in range(0,NUM_FACES, 1):
-  print("#########")
-  print("#       #")
-  print("# O   O #")
-  print("|   V   |")
-  print("|  ---  |")
-  print("|_______|")
-  
+for count in range(0, NUM_FACES, 1):
+    print("#########")
+    print("#       #")
+    print("# O   O #")
+    print("|   V   |")
+    print("|  ---  |")
+    print("|_______|")
