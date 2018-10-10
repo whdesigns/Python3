86eff5a845367801103749260721a5e783177977
William Hughes <43645293+whdesigns@users.noreply.github.com>
2018-10-10 19:49:05 +0100

    Create bot.py
    Used a while loop to count from 1-100 in ascii art.

---
+++ 1-basics/4-repetition1/1-while-loop/bot.py
@@ -0,0 +1,10 @@
+NUM_FACES = 100
+
+for count in range(0,NUM_FACES, 1):
+  print("#########")
+  print("#       #")
+  print("# O   O #")
+  print("|   V   |")
+  print("|  ---  |")
+  print("|_______|")
+  
