9ad1823df572c57a0f006ddf293f3358fcbe64e6
William Hughes <43645293+whdesigns@users.noreply.github.com>
2018-10-11 16:44:55 +0100

    Create bot.py

---
+++ 1-basics/4-repetition1/5-sum-user-10/bot.py
@@ -0,0 +1,8 @@
+total = 0
+for count in range(0, 10, 1):
+  print("Please enter a number:")
+  user_num = int(input())
+  total = total + user_num
+
+print(total)
+print("The sum of your 10 numbers is", total)
