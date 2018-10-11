4299a76c6dc8ccb4b802837c54d0eb451be28002
William Hughes <43645293+whdesigns@users.noreply.github.com>
2018-10-11 15:18:50 +0100

    Update bot.py
    For loop to determine how many ascii art to print.

--- 1-basics/4-repetition1/2-for-loop/bot.py
+++ 1-basics/4-repetition1/2-for-loop/bot.py
@@ -1,7 +1,21 @@
-for count in range(0, 10):
+max_robots = 10
+
+print("Enter a number of robots:")
+num_robots = int(input())
+
+if(num_robots < max_robots):
+  for count in range(0,num_robots,1):
     print("#########")
     print("#       #")
     print("# O   O #")
     print("|   V   |")
     print("|  ---  |")
     print("|_______|")
+else:
+  for count in range(0, max_robots, 1):
+      print("#########")
+      print("#       #")
+      print("# O   O #")
+      print("|   V   |")
+      print("|  ---  |")
+      print("|_______|")
