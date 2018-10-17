e518df76be595327d848ad453d8c3bd750f82bb3
William Hughes <43645293+whdesigns@users.noreply.github.com>
2018-10-17 19:30:12 +0100

    Create bot.py
    A basic greeting exercise to demonstrate how to use functions.

---
+++ 1-basics/5-functions/1-greeting/bot.py
@@ -0,0 +1,8 @@
+def greet_user(): # Declaring a function called greet_user. 
+  print("Please enter your name")
+  name = str(input()) # Asking the user to enter their name.
+  print("Hello", name) 
+
+greet_user()
+greet_user()
+greet_user() # This is duplicated to print the question 3 times. 
