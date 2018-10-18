1e2adab7fe2592b514ca41a10281c76d8f9353af
William Hughes <43645293+whdesigns@users.noreply.github.com>
2018-10-18 14:45:33 +0100

    Create bot.py

---
+++ 1-basics/5-functions/2-leisure/bot.py
@@ -0,0 +1,15 @@
+def leisure_time():
+  print("Please enter an activity:")
+  leisure_time = str(input())
+
+  if leisure_time == "watch movie":
+    print("watching a movie sounds like fun")
+
+  elif leisure_time == "play":
+    print("I have lots of toys to play with.")
+
+  else:
+    print("I am not sure if I will like it but I will give it a try.")
+
+
+leisure_time()
