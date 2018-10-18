40eebb1f8aa66bece64d1612f663617c77428398
William Hughes <43645293+whdesigns@users.noreply.github.com>
2018-10-18 15:52:14 +0100

    Create bot.py

---
+++ 1-basics/5-functions/5-matilda/bot.py
@@ -0,0 +1,18 @@
+def determine_order_of_books(first_book, second_book):
+  if (first_book < second_book):
+    print("Order is first book followed by second book")
+  elif (second_book < first_book):
+    print("Order is second book followed by first book")
+  else:
+    print("The order does not matter")
+
+
+
+determine_order_of_books(1, 2)
+determine_order_of_books(7, 5)
+determine_order_of_books(1, 3)
+determine_order_of_books(4, 4)
+	
+
+
+
