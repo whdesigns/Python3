f6096437da785731d62b038b2b69977aba881f2b
William Hughes <43645293+whdesigns@users.noreply.github.com>
2018-10-18 15:06:04 +0100

    Create bot.py

---
+++ 1-basics/5-functions/3-cinderella/bot.py
@@ -0,0 +1,18 @@
+def try_glass_slipper_on(person):
+
+  if person == "Eldest step sister":
+    print("You are not my princess. Your feet are too big!")
+  elif person == "Youngest step sister":
+    print("You are not my princess. Your feet are too small!")
+  elif person == "Cinderella":
+    print("I found my princess!")
+  else: 
+    print("You are not my princess!")
+try_glass_slipper_on("Cinderella")
+    
+
+try_glass_slipper_on("Cinderella")
+try_glass_slipper_on("Eldest step sister")
+try_glass_slipper_on("Youngest step sister")
+
+	
