d951772e432f64de9ccb24bcd381e25526d07434
William Hughes <43645293+whdesigns@users.noreply.github.com>
2018-10-19 22:09:31 +0100

    Update bot.py

--- 1-basics/4-repetition1/mock-question/bot.py
+++ 1-basics/4-repetition1/mock-question/bot.py
@@ -1,8 +1,5 @@
 print("Please enter a whole number:")
-whole_number = int(input())
+user_number = int(input())
 print("Counting Down....")
-count = 0
-for whole_number in range(whole_number, -1, -1):
-   print(whole_number)
-
-    
+for number in range(user_number, -1, -1):
+   print(number)
