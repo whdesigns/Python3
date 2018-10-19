8b52eba0e2eac3d3c369fb9761ad6ca75713d7a8
William Hughes <43645293+whdesigns@users.noreply.github.com>
2018-10-19 17:33:40 +0100

    Create bot.py

---
+++ 1-basics/5-functions/mock_question/bot.py
@@ -0,0 +1,16 @@
+def displaySmallest(num1, num2):
+  if (num1 < num2):
+    print("The first number is smaller")
+  elif (num2 < num1):
+    print("The second number is smaller")
+  else: 
+    print("Both numbers are equal")
+
+
+displaySmallest (7, 5)
+displaySmallest (1, 3)
+displaySmallest (4, 4)
+
+#The second number is smaller
+#The first number is smaller
+#Both numbers are equal
