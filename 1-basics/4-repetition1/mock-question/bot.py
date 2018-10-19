0b5aba8045dde6c5315153adbce1b43141fc13e6
William Hughes <43645293+whdesigns@users.noreply.github.com>
2018-10-19 21:46:12 +0100

    Create bot.py

---
+++ 1-basics/4-repetition1/mock-question/bot.py
@@ -0,0 +1,8 @@
+print("Please enter a whole number:")
+whole_number = int(input())
+print("Counting Down....")
+count = 0
+for whole_number in range(whole_number, -1, -1):
+   print(whole_number)
+
+    
