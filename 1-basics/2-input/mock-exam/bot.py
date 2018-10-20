f1b3fc932f85138dc13d92d664c879331470083a
William Hughes <43645293+whdesigns@users.noreply.github.com>
2018-10-20 13:14:47 +0100

    Create bot.py

---
+++ 1-basics/2-input/mock-exam/bot.py
@@ -0,0 +1,8 @@
+print("Please enter your name: ") 
+# The above code uses the string datatype to ask the the user what his/her name is.
+
+name = input() 
+# "name" is assigned as a variable, because of the "=" sign. The variable is equal to input, meaning that whatever the user types in will be stored in this variable. In other words, name is equal to the value of input. I closed the input function with paretheses so the value will be taken and stored correctly.
+
+print("Hello", name)
+#This displays the message "Hello" in string, followed by the variable, which will be the name the user typed and that was stored inside it earlier. 
