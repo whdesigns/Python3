45d056c7012248c1892ee8715aa7324497584740
William Hughes <43645293+whdesigns@users.noreply.github.com>
2018-10-24 12:29:12 +0100

    Create bot.py

---
+++ 1-basics/3-decision/mock-exam-3/bot.py
@@ -0,0 +1,6 @@
+print("Have you fear in your heart?")
+user_answer = str(input())
+if user_answer == "yes":
+  print("Fear is the path to the dark side. You cannot be a Jedi apprentice.")
+else: 
+  print("The force is strong in you. You may be a Jedi apprentice.")
