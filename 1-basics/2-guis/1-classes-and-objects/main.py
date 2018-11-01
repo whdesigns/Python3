69034207e2d67bf69da6df76324e2206654a1c31
William Hughes <43645293+whdesigns@users.noreply.github.com>
2018-11-01 16:39:58 +0000

    Create main.py
    Added code to create an object of Bot class

---
+++ 1-basics/2-guis/1-classes-and-objects/main.py
@@ -0,0 +1,12 @@
+# These are the objects 
+from bot import Bot
+
+beep = Bot("Beep")
+
+beep.display_name()
+beep.display_age()
+beep.display_energy()
+beep.display_shield()
+beep.display_summary()
+
+
