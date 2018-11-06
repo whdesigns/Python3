c15564968e392022bb796f31ea5bdb32efd7c633
William Hughes <43645293+whdesigns@users.noreply.github.com>
2018-11-06 21:21:40 +0000

    Create bot.py

---
+++ 1-basics/2-guis/1-classes-and-objects/Multiple-Inheritance3/bot.py
@@ -0,0 +1,19 @@
+class A:
+  def __init__(self, name, occupation):
+    self.name = name 
+    self.occupation = occupation
+    print("Assignment due soon :(")
+    
+class A:
+  def option(self):
+    print("testing")
+
+class C(A):
+  def option2(self):
+    print("testing2")
+
+
+c = C()
+c.option()
+c.option2()
+
