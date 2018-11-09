78912e68284480e605771b41deda8f6301cd36ee
William Hughes <43645293+whdesigns@users.noreply.github.com>
2018-11-09 23:12:29 +0000

    Create bot.py

---
+++ 1-basics/2-guis/1-classes-and-objects/Basic-Tkinter-pack/bot.py
@@ -0,0 +1,6 @@
+from tkinter import * 
+
+root = Tk()  # Creates a blank window using the tkinter function
+theLabel = Label(root, text="Too easy") # assigning a variable equal to Label, meaning the variable will be a Label datatype
+theLabel.pack() # "pack" places the Label anywhere it'll fit on the screen
+root.mainloop() # runs programs
